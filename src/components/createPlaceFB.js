aebd7ea14586f2af1524b36f48b302ebcd7a1574
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-02-17 15:02:51 +0100

    add image upload feature

--- src/components/createPlaceFB.js
+++ src/components/createPlaceFB.js
@@ -66,6 +66,13 @@ class CreatePlaceFB extends React.Component{
         this.setState({[event.target.name]:event.target.value})
     }
 
+    handleImageChange = event => {
+        if (event.target.files[0]) {
+            const image = event.target.files[0];
+            this.setState(() => ({ image }))
+        }
+    }
+
     keepHookID=e=>{
         let count = this.state.count
         let hook_state = this.state.place_hooks
@@ -88,20 +95,32 @@ class CreatePlaceFB extends React.Component{
     }
 
     onPlaceUpdate=(userId,place_name,description,image,place_hooks,place_id,contact,ig_acct,youtube_term,email,subaccount)=>{
-        this.props.firebase.place(place_id).set({
-            userId:userId,
-            place_name:place_name,
-            description:description,
-            contact:contact,
-            ig_acct:ig_acct,
-            youtube_term:youtube_term,
-            email:email,
-            subaccount:subaccount,
-            image:image,
-            place_hooks:place_hooks,
+        this.props.firebase.imageStore(image.name).put(image).on("state_changed", snapShot => {
+            //get the progress
+            const progress = Math.round((snapShot.bytesTransfered / snapShot.totalBytes) * 100)
+
+            this.setState({ progress });
+        }, error => {
+            console.log(error)
+        }, () => {
+            //complete the image storage
+            this.props.firebase.imageStores().child(image.name).getDownloadURL().then(url => {
+                this.props.firebase.place(place_id).set({
+                userId:userId,
+                place_name:place_name,
+                description:description,
+                contact:contact,
+                ig_acct:ig_acct,
+                youtube_term:youtube_term,
+                email:email,
+                subaccount:subaccount,
+                image:url,
+                place_hooks:place_hooks,
             
+            })
+            this.props.firebase.sendEmail(update_subject, place_name, url , description);
         })
-        this.props.firebase.sendEmail(update_subject, place_name, image, description);
+    })
 
     }
 
@@ -110,27 +129,39 @@ class CreatePlaceFB extends React.Component{
         const isPlaceName=place_name!==""
         const isDescription=description!==""
         const isContact=contact!==""
-        const isSubaccount=subaccount!==""
+        //const isSubaccount=subaccount!==""
         const isImage=image!==""
         const isPlaceHooks=place_hooks!==null
-        const proceedToSubmit=isPlaceName && isDescription && isContact && isImage && isPlaceHooks && isSubaccount
+        const proceedToSubmit=isPlaceName && isDescription && isContact && isImage && isPlaceHooks 
         if(proceedToSubmit){
-            this.props.firebase.places().push({
-                userId:authUser.uid,
-                place_name:place_name,
-                description:description,
-                contact:contact,
-                ig_acct:ig_acct,
-                youtube_term:youtube_term,
-                email:email,
-                subaccount:subaccount,
-                image:image,
-                place_hooks:place_hooks,
-            })
-            this.setState({isCreated:true})
-            //send mail to all registered users about new created place
-            this.props.firebase.sendEmail(subject, place_name, image, description);
-            
+            //upload the image
+            this.props.firebase.imageStore(image.name).put(image).on("state_changed", snapShot => {
+                //get the progress
+                const progress = Math.round((snapShot.bytesTransfered / snapShot.totalBytes) * 100)
+
+                this.setState({ progress });
+            }, error => {
+                console.log(error)
+            }, () => {
+                //complete the image storage
+                this.props.firebase.imageStores().child(image.name).getDownloadURL().then(url => {
+                    this.props.firebase.places().push({
+                    userId:authUser.uid,
+                    place_name:place_name,
+                    description:description,
+                    contact:contact,
+                    ig_acct:ig_acct,
+                    youtube_term:youtube_term,
+                    email:email,
+                    subaccount:subaccount,
+                    image:url,
+                    place_hooks:place_hooks,
+                })
+                this.setState({isCreated:true})
+                //send mail to all registered users about new created place
+                this.props.firebase.sendEmail(subject, place_name, url, description);
+                })
+            })   
         }
         else{
             this.setState({isCreated:false})
@@ -138,8 +169,7 @@ class CreatePlaceFB extends React.Component{
     }
 
     render(){
-        const {user_id,place_name,description,contact,ig_acct,youtube_term,email,subaccount,image,place_hooks,hooks,loading_hooks,loading_places,isCreated,places}=this.state
-        console.log(places)
+        const {user_id,place_name,description,contact,ig_acct,youtube_term,email,subaccount,place_hooks,hooks,loading_hooks,loading_places,isCreated,places}=this.state
         return(
                 
                 <div id="create-place">
@@ -179,7 +209,8 @@ class CreatePlaceFB extends React.Component{
                                 <input type="text" name="subaccount" placeholder="enter subaccount number" value={subaccount} className="form-control" onChange={this.onChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" name="image" placeholder="enter image url" className="form-control" value={image} onChange={this.onChange} />
+                                <input type="file" id="myFile" className="form-control" name="image" onChange={this.handleImageChange} />
+                                {/* <input type="text" name="image" placeholder="enter image url" className="form-control" value={image} onChange={this.onChange} /> */}
                             </div>
                             <p className="bg-dark text-white">What hooks relate to this offer?</p>
                             <div className="form-check-inline">
@@ -237,6 +268,15 @@ class PlaceTemplate extends React.Component{
     onChange=event=>{
         this.setState({[event.target.name]:event.target.value})
     }
+
+    handleImageChange = event => {
+        if (event.target.files[0]) {
+            const image = event.target.files[0];
+            this.setState(() => ({ image }))
+        }
+    }
+
+
 
     onDeletePlace=()=>{
         this.props.onDeletePlace(this.state.place_id)
@@ -299,7 +339,7 @@ class PlaceTemplate extends React.Component{
                             <input type="text" name="youtube_term" placeholder="enter youtube term" value={youtube_term} className="form-control" onChange={this.onChange} />
                         </div>
                     <div className="form-group">
-                        <input type="text" name="email" placeholder="enter email" value={email} className="form-control" onChange={this.onChange} />
+                            <input type="file" id="myFile" className="form-control" name="image" onChange={this.handleImageChange} />
                     </div>
                     <div className="form-group">
                         <input type="text" name="subaccount" placeholder="enter subaccount number" value={subaccount} className="form-control" onChange={this.onChange} />
