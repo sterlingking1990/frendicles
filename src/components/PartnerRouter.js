937fc6cbfbb4e26eaa1a6c4b8948f1301eea3191
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-02-17 15:08:39 +0100

    change path name for route

--- src/components/PartnerRouter.js
+++ src/components/PartnerRouter.js
@@ -4,14 +4,14 @@ import Dashboard from './Dashboard';
 import ErrorPage from '../components/ErrorPage';
 import PlacesToJoinFB from './PlacesToJoinFB';
 import AcceptTransactionFB from './AcceptTransactionFB';
-import SetFunLimitFB from './SetFunLimitFB';
-import FunSlotsFB from './FunSlotsFB';
+import SetRewardLimitFB from './SetRewardLimitFB';
 import ChangePasswordPage from './ChangePasswordPage';
 import createPlaceFB from './createPlaceFB';
-import SetFunTypeFB from './SetFunTypeFB';
+import SetRewardTypeFB from './SetRewardTypeFB';
 import MyTransactionsFB from './MyTransactionsFB';
 import OfatriBalanceFB from './OfatriBalanceFB';
 import VerifyTransaction from './VerifyTransaction';
+import AdminSetupGoal from './AdminSetupGoal';
 
 const UserRouter = () => (
     <Switch>
@@ -21,9 +21,9 @@ const UserRouter = () => (
         <Route exact path="/my-transactions" component={MyTransactionsFB} />
         <Route exact path="/accept-transaction" component={AcceptTransactionFB} />
         <Route exact path="/verify-transaction" component={VerifyTransaction} />
-        <Route exact path="/set-fun-limit" component={SetFunLimitFB} />
-        <Route exact path="/set-fun-type" component={SetFunTypeFB} />
-        <Route exact path="/fun-slots" component={FunSlotsFB} />
+        <Route exact path="/admin-setup-goal" component={AdminSetupGoal} />
+        <Route exact path="/set-reward-limit" component={SetRewardLimitFB} />
+        <Route exact path="/set-reward-type" component={SetRewardTypeFB} />
         <Route exact path="/ofatri-balance" component={OfatriBalanceFB} />
         <Route exact path="/change-password" component={ChangePasswordPage} />
         <Route exact component={ErrorPage} />
