9207d79e338cc174caf246985603356033b93fe4
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-05 15:17:50 +0100

    make menu collapse after activated

--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -2,55 +2,104 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 import SignOutUser from './SignOutUser'
 const AdminHeader = () => (
-    <div id="header">
-        <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-            <div className="container mt-3">
-                <button className="navbar-toggler" data-target="#Navmenu" data-toggle="collapse">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  <div id="header">
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+      <div className="container mt-3">
+        <button
+          className="navbar-toggler"
+          data-target="#Navmenu"
+          data-toggle="collapse"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-                <a href="/" className="navbar-brand"><span className="navbar-logo">ofatri</span></a>
+        <a href="/" className="navbar-brand">
+          <span className="navbar-logo">ofatri</span>
+        </a>
 
-                <div className="collapse navbar-collapse" id="Navmenu">
-                    <ul className="navbar-nav ml-auto">
-                        {/* <li className="nav-item">
+        <div className="collapse navbar-collapse" id="Navmenu">
+          <ul className="navbar-nav ml-auto">
+            {/* <li className="nav-item">
                         <NavLink to="/hooks-page" className="nav-link">Hooks Page </NavLink>
                         </li> */}
-                        <li className="nav-item">
-                        <NavLink to="/confirm-funcode" className="nav-link">Confirm FunCode</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to="/redeem-coin" className="nav-link">Redeem Coin</NavLink>
-                        </li>
-                        {/* <li className="nav-item">
+            <li>
+              <NavLink
+                to="/confirm-funcode"
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#Navmenu"
+              >
+                Confirm FunCode
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/redeem-coin"
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#Navmenu"
+              >
+                Redeem Coin
+              </NavLink>
+            </li>
+            {/* <li className="nav-item">
                             <NavLink to="/places-page" className="nav-link">Places Page </NavLink>
                         </li> */}
-                        {/* <li className="nav-item">
+            {/* <li className="nav-item">
                             <NavLink to="/create-hook" className="nav-link">Create Hook</NavLink>
                         </li> */}
-                        <li className="nav-item">
-                            <NavLink to="/admin-setup-goal" className="nav-link">Setup Goals</NavLink>
-                        </li>
-  
-                        <li className="dropdown" id="menu-anchor">
-                            <a className="dropdown-toggle" data-toggle="dropdown" href="#">Account
-                                <span className="caret"></span>
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li className="nav-item"><NavLink className="nav-link" to="/update-ofatri-account" activeClassName="is-active" exact={true}>Update Ofatri Account</NavLink></li>
-                                <li className="nav-item"><NavLink className="nav-link" to="/users-info" activeClassName="is-active" exact={true}>User Info</NavLink></li>
-                            </ul>
-                        </li>
+            <li>
+              <NavLink
+                to="/admin-setup-goal"
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#Navmenu"
+              >
+                Setup Goals
+              </NavLink>
+            </li>
 
-                        <li className="nav-item">
-                            <SignOutUser/>
-                        </li>
+            <li className="dropdown" id="menu-anchor">
+              <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+                Account
+                <span className="caret"></span>
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <NavLink
+                    className="nav-link"
+                    data-toggle="collapse"
+                    data-target="#Navmenu"
+                    to="/update-ofatri-account"
+                    activeClassName="is-active"
+                    exact={true}
+                  >
+                    Update Ofatri Account
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    className="nav-link"
+                    data-toggle="collapse"
+                    data-target="#Navmenu"
+                    to="/users-info"
+                    activeClassName="is-active"
+                    exact={true}
+                  >
+                    User Info
+                  </NavLink>
+                </li>
+              </ul>
+            </li>
 
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-)
+            <li className="nav-item">
+              <SignOutUser />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+);
 
 export default AdminHeader
