3fcad868434aad25a50ec1a403abbe40d560d18a
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-05 15:17:24 +0100

    make menu collapse back after click

--- src/components/GuestHeader.js
+++ src/components/GuestHeader.js
@@ -1,22 +1,50 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 const GuestHeader = () => (
-    <div id="header">
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-            <div className="container">
-                <button className="navbar-toggler" data-toggle="collapse" data-target="#NavbarMenu">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <a href="/" className="navbar-brand"><span className="navbar-logo">ofatri</span></a>
-                <div className="collapse navbar-collapse" id="NavbarMenu">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item"><NavLink className="nav-link" to="/signup" activeClassName="is-active" exact={true}>Sign Up</NavLink></li>
-                        <li className="nav-item"><NavLink className="nav-link" to="/signin" activeClassName="is-active" exact={true}>Sign In</NavLink></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-)
+  <div id="header">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+      <div className="container">
+        <button
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#NavbarMenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a href="/" className="navbar-brand">
+          <span className="navbar-logo">ofatri</span>
+        </a>
+        <div className="collapse navbar-collapse" id="NavbarMenu">
+          <ul className="navbar-nav ml-auto">
+            <li>
+              <NavLink
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#NavbarMenu"
+                to="/signup"
+                activeClassName="is-active"
+                exact={true}
+              >
+                Sign Up
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="nav-link"
+                data-toggle="collapse"
+                data-target="#NavbarMenu"
+                to="/signin"
+                activeClassName="is-active"
+                exact={true}
+              >
+                Sign In
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+);
 
 export default GuestHeader
