ebe94d24ef44324f63b9b302c772ba73ac183f58
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-26 23:46:55 +0100

    fix navigation for route

--- src/components/PartnerRouter.js
+++ src/components/PartnerRouter.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import ErrorPage from '../components/ErrorPage';
 import PlacesToJoinFB from './PlacesToJoinFB';
+import OfferAnalytics from "./OfferAnalytics";
 import AcceptTransactionFB from './AcceptTransactionFB';
 import SetRewardLimitFB from './SetRewardLimitFB';
 import ChangePasswordPage from './ChangePasswordPage';
@@ -15,22 +16,25 @@ import AdminSetupGoal from './AdminSetupGoal';
 import ConfirmFunCodeFB from './ConfirmFunCodeFB';
 
 const UserRouter = () => (
-    <Switch>
-        <Route exact path="/" component={Dashboard} />
-        <Route exact path="/create-offer" component={createPlaceFB} />
-        <Route exact path="/offer" component={PlacesToJoinFB} />
-        <Route exact path="/my-transactions" component={MyTransactionsFB} />
-        <Route exact path="/offer-reward" component={AcceptTransactionFB} />
-        <Route exact path="/close-reward" component={ConfirmFunCodeFB} />
-        <Route exact path="/verify-payment" component={VerifyTransaction} />
-        <Route exact path="/admin-setup-goal" component={AdminSetupGoal} />
-        <Route exact path="/set-reward-limit" component={SetRewardLimitFB} />
-        <Route exact path="/set-reward-type" component={SetRewardTypeFB} />
-        <Route exact path="/ofatri-balance" component={OfatriBalanceFB} />
-        <Route exact path="/change-password" component={ChangePasswordPage} />
-        <Route exact component={ErrorPage} />
-    </Switch>
-)
+  <Switch>
+    <Route exact path="/" component={Dashboard} />
+    <Route exact path="/create-offer" component={createPlaceFB} />
+    {/* <Route exact path="/offer" component={PlacesToJoinFB} /> */}
+    <Route exact path="/admin-setup-goal" component={AdminSetupGoal} />
+    <Route exact path="/change-password" component={ChangePasswordPage} />
+    <Route exact path="/offer-analytics" component={OfferAnalytics} />
+    {/* <Route exact path="/my-transactions" component={MyTransactionsFB} />
+    <Route exact path="/offer-reward" component={AcceptTransactionFB} />
+    <Route exact path="/close-reward" component={ConfirmFunCodeFB} /> */}
+    {/* <Route exact path="/verify-payment" component={VerifyTransaction} /> */}
+
+    {/* <Route exact path="/set-reward-limit" component={SetRewardLimitFB} />
+    <Route exact path="/set-reward-type" component={SetRewardTypeFB} /> */}
+    {/* <Route exact path="/ofatri-balance" component={OfatriBalanceFB} /> */}
+
+    <Route exact component={ErrorPage} />
+  </Switch>
+);
 
 export default UserRouter
 
