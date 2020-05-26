e520a1389f0c56ad8e377691ca7da6131d709f9b
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-05-26 23:50:00 +0100

    remove offer analytics feature for user

--- src/components/UserRouter.js
+++ src/components/UserRouter.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {Switch,Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import ErrorPage from './ErrorPage';
 import PlacesToJoinFB from './PlacesToJoinFB';
 import ChangePasswordPage from './ChangePasswordPage';
-import MyTransactionsFB from './MyTransactionsFB';
+// import MyTransactionsFB from './MyTransactionsFB';
 import SetRewardGoal from './SetRewardGoal';
 import CheckMilestone from './CheckMilestone';
-import OfferAnalytics from './OfferAnalytics';
+
 import SetNotification from './SetNotification';
 import ChangePhone from './ChangePhone';
 
 const UserRouter = () => (
-            <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/offer" component={PlacesToJoinFB} />
-                <Route exact path="/my-transactions" component={MyTransactionsFB} />
-                <Route exact path="/change-password" component={ChangePasswordPage} />
-                <Route exact path="/change-phone" component={ChangePhone} />
-                <Route exact path="/set-notification" component={SetNotification}/>
-                <Route exact path="/set-reward-goal" component={SetRewardGoal} />
-                <Route exact path="/check-milestone" component={CheckMilestone} />
-                <Route exact path="/offer-analytics" component={OfferAnalytics} />
+  <Switch>
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/offer" component={PlacesToJoinFB} />
+      {/* <Route exact path="/my-transactions" component={MyTransactionsFB} /> */}
+      <Route exact path="/change-password" component={ChangePasswordPage} />
+      <Route exact path="/change-phone" component={ChangePhone} />
+      <Route exact path="/set-notification" component={SetNotification}/>
+      <Route exact path="/set-reward-goal" component={SetRewardGoal} />
+      <Route exact path="/check-milestone" component={CheckMilestone} />
+      
+      <Route exact  component={ErrorPage} />
 
-                <Route exact component={ErrorPage} />
-            </Switch>
-)
+  </Switch>
+
+
+);
 
 export default UserRouter
 
