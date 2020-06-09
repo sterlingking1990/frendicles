69716d47ab09ce2f9ae0b6a862edfc84e94cc338
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-06-09 16:02:19 +0100

    use zen loader and remove semi colon

--- src/components/Loadable.js
+++ src/components/Loadable.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
+// import Loader from "react-loader-spinner";
+import { ThemeProvider } from "@zendeskgarden/react-theming";
+import { Dots } from "@zendeskgarden/react-loaders";
 
 export default class Loadable extends React.Component {
     constructor(props){
@@ -9,15 +11,18 @@ export default class Loadable extends React.Component {
   //other logic
   render() {
     return (
-    <div id="centralise">
-                        <Loader
+      <div id="centralise">
+        <ThemeProvider>
+          <Dots />
+        </ThemeProvider>
+        {/* <Loader
                             type="Hearts"
                             color="#00BFFF"
                             height={100}
                             width={100}
                             timeout={3000} //3 secs
-                        />
-    </div>
+                        /> */}
+      </div>
     );
   }
 }
