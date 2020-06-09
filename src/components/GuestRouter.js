063d9852e3fae6cae54e619a32c6831d09605e40
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-06-09 09:21:15 +0100

    add lazy loading to landing page

--- src/components/GuestRouter.js
+++ src/components/GuestRouter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import LandingPage from '../components/LandingPage';
+import LoadableHome from '../components/LoadableHome';
 import ErrorPage from '../components/ErrorPage';
 import SignUpPage from '../components/SignUpPage';
 import SignInPage from '../components/SignInPage';
 import ForgetPasswordPage from '../components/ForgetPasswordPage';
 const GuestRouter = () => (
             <Switch>
-                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/" component={LoadableHome} />
                 <Route exact path="/signup" component={SignUpPage} />
                 <Route exact path="/signin" component={SignInPage} />
                 <Route exact path="/recover-password" component={ForgetPasswordPage}/>
