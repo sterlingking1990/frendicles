d3a8801d53f175faeba5d5e476f83504d34f696b
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-06-10 01:40:58 +0100

    deleted unused files

--- src/Visibility/Hooks/hooks.js
+++
@@ -1,9 +0,0 @@
-//HOOKS VISIBILITY
-export const getHooksVisibility = (hookReducer, { hook_text }) => {
-    return hookReducer.filter((hook) => hook.description.toLowerCase().includes(hook_text.toLowerCase()))
-}
-
-export const getHooksToJoinVisibility = (hookReducer, { hook_text }) => {
-    return hookReducer.filter((hook) => hook.description.toLowerCase().includes(hook_text.toLowerCase()))
-}
-
