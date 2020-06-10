d86cd07898c94bb69ec5939f10f5414591993eaa
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-06-10 23:34:05 +0100

    add user query to authentication

--- src/session/withAuthentication.js
+++ src/session/withAuthentication.js
@@ -8,22 +8,34 @@ const withAuthentication = Component=>{
             super(props);
             this.state={
                 authUser:null,
+                users:[]
             }
         }
 
         componentDidMount() {
             this.props.firebase.auth.onAuthStateChanged(authUser => {
                 authUser ? this.setState({ authUser }) : this.setState({ authUser: null })
+
+                this.props.firebase.users().on("value", snapShot => {
+                  const userObj = snapShot.val();
+                  if(userObj){
+                  const userArr = Object.keys(userObj).map(key => ({
+                    ...userObj[key],uid:key
+                  }))
+                  console.log(userArr)
+                  this.setState({ users: userArr });
+                }
+                });
             });
+            
         }
 
         render(){
             return (
-            <AuthUserContext.Provider value={this.state.authUser}>
+            <AuthUserContext.Provider value={{authState:this.state.authUser,users:this.state.users}}>
             <Component {...this.props} />
             </AuthUserContext.Provider>
             )
-
         }
     }
     return withFirebase(withAuthentication)
