66fccc3ec9e4d5d11ea5cf56c32dd03cfb57d83c
simplyking-decadev <56389903+simplyking-decadev@users.noreply.github.com>
2020-06-10 01:38:12 +0100

    notify user that mail sent after sign up

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -40,7 +40,6 @@ class SignUpHOC extends Component {
           //set time out before redirecting to home page---after 2000 secs, then redirect...by then you must have used the send mail(true) to notify user that email
           //has been sent to his mail, please check your mail to know about best tips to win rewards...
           setTimeout(function(){
-            this.setState({send_mail:false})
             history.push('/')
             history.go(0)
           }.bind(this), 3000);
@@ -65,52 +64,122 @@ class SignUpHOC extends Component {
         const isInvalid = password !== confirmPassword || password === '' || email === '' || phone === '' || username ==='';
 
         return (
-
-            <div className="signup">
-               <div className="banner-body-background">
-                    <div className="banner-body-text1">
-                        <span className="logo-name" id="app-name">ofatri</span>
-                        <div className="text-display text-center"><strong id="first_heading">Make Transactions </strong> &nbsp;<strong id="second_heading"> Get Rewarded</strong>&nbsp;<strong id="third_heading"> Achieve Goals</strong></div>
-                    </div>
-                    <div className="sign-up-flow">
-                        <div className="container mt-3">
-                            <div className="row">
-                                <div className="col-lg-12 sm-12">
-                                    {error && <h3 className="text-display text-white bg-dark">{error}</h3>}
-                                    {send_mail && <h3 className="text-display text-white bg-dark">Successfully signed up, check your email for more on getting rewards</h3>}
-                                    <div className="form-group">
-                                        <input name="username" className="form-control" type="text" placeholder="enter username" onChange={this.onChange} value={username} required />
-                                    </div>
-                                    <div className="form-group">
-                                        <input name="email" className="form-control" type="email" placeholder="enter your valid email address" onChange={this.onChange} value={email} required />
-                                    </div>
-                                    <div className="form-group">
-                                        <input name="phone" className="form-control" type="number" placeholder="enter your valid phone number" onChange={this.onChange} value={phone} required/>
-                                    </div>
-                                    <div className="form-group">
-                                        <input name="password" className="form-control" type="password" placeholder="enter your password" onChange={this.onChange} value={password} required/>
-                                    </div>
-                                    <div className="form-group">
-                                        <input name="confirmPassword" className="form-control" type="password" placeholder="re-enter password to confirm" onChange={this.onChange} value={confirmPassword} required/>
-                                    </div>
-                                    <div className="form-group">
-                                        <button className="form-control btn btn-success text-white" disabled={isInvalid} onClick={this.onSubmit}>Sign Up</button>
-                                    </div>
-                                    <div className="form-group">
-                                        {error && <p className="text-muted text-red bg-white">{error.message}</p>}
-                                    </div>
+          <div className="signup">
+            <div className="banner-body-background">
+              <div className="banner-body-text1">
+                <span className="logo-name" id="app-name">
+                  ofatri
+                </span>
+                <div className="text-display text-center">
+                  <strong id="first_heading">Make Transactions </strong> &nbsp;
+                  <strong id="second_heading"> Get Rewarded</strong>&nbsp;
+                  <strong id="third_heading"> Achieve Goals</strong>
+                </div>
+              </div>
+              <div className="sign-up-flow">
+                <div className="container mt-3">
+                  <div className="row">
+                    <div className="col-lg-12 sm-12">
+                          {error && (
+                              <div className="card bg-dark">
+                                <div className="card-body">
+                                    <p className="text-display text-center text-white">
+                                        {error}
+                                    </p>
                                 </div>
                             </div>
-                            {/* <div className="row">
+                            )}
+                          {send_mail && (
+                              <div className="card bg-dark">
+                                <div className="card-body">
+                                    <p className="text-display text-center text-white">
+                                    Successfully signed up, check your email for more
+                                    on getting rewards
+                                    </p>
+                                </div>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                      <div className="form-group">
+                        <input
+                          name="username"
+                          className="form-control"
+                          type="text"
+                          placeholder="enter username"
+                          onChange={this.onChange}
+                          value={username}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <input
+                          name="email"
+                          className="form-control"
+                          type="email"
+                          placeholder="enter your valid email address"
+                          onChange={this.onChange}
+                          value={email}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <input
+                          name="phone"
+                          className="form-control"
+                          type="number"
+                          placeholder="enter your valid phone number"
+                          onChange={this.onChange}
+                          value={phone}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <input
+                          name="password"
+                          className="form-control"
+                          type="password"
+                          placeholder="enter your password"
+                          onChange={this.onChange}
+                          value={password}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <input
+                          name="confirmPassword"
+                          className="form-control"
+                          type="password"
+                          placeholder="re-enter password to confirm"
+                          onChange={this.onChange}
+                          value={confirmPassword}
+                          required
+                        />
+                      </div>
+                      <div className="form-group">
+                        <button
+                          className="form-control btn btn-success text-white"
+                          disabled={isInvalid}
+                          onClick={this.onSubmit}
+                        >
+                          Sign Up
+                        </button>
+                      </div>
+                      <div className="form-group">
+                        {error && (
+                          <p className="text-muted text-red bg-white">
+                            {error.message}
+                          </p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div className="row">
                                 <SignInFacebook/>
                             </div> */}
-                        </div>
-                    </div>
                 </div>
-
-            
-        </div>
-        )
+              </div>
+        );
 
     }
 }
